1f7c1ea6510928ff3731ad5936041fc045012530
diana <daianna93@gmail.com>
2015-05-19 13:55:38 +0200

    add portfolio

---
+++ jquery.js
@@ -0,0 +1,12 @@
+<script src="typed.js"></script>
+<script>
+  $(function(){
+      $(".element").typed({
+        strings: ["First sentence.", "Second sentence."],
+        typeSpeed: 0
+      });
+  });
+</script>
+...
+
+<div class="element"></div>
